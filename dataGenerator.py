08ea7089b86f81188dfc70072c55ac59a9a0dd6e
David Angeles Ungson <davidangelesu@gmail.com>
2024-03-06 18:34:04 +0100

    modify how data is generated

--- dataGenerator.py
+++ dataGenerator.py
@@ -1,6 +1,23 @@
 import numpy as np
 import pyvista as pv
+import os
 
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+        print("Folder %s created!" % path)
+    else:
+        print("Folder %s already exists" % path)
+
+basePath="./data"
+
+# 2D Data Creation
+
+
+path2DData = basePath+'/2D'
+
+create_dir(path2DData)
 
 
 grid= pv.ImageData(dimensions=(41, 41, 1), spacing=(.5, .5, .5),origin=(-10,-10,0))
@@ -9,15 +26,26 @@ r=np.sqrt(grid.x**2+grid.y**2)
 phases=np.linspace(0,2*np.pi,15)
 
 
-
 for timestep in range(0,15):
+
+    path=path2DData+'/reference'
+
+    if timestep==0:
+        create_dir(path)
 
     phase=phases[timestep]
     z=np.sin(r+phase)
     grid['Height']=z
-    grid.save('data/mesh_i_0_t_{}.vti'.format(timestep))
+    print(grid)
+    grid.save(path+'/mesh_t_{}.vti'.format(timestep))
 
     for i,scale in enumerate([0.01,0.05,0.1]):
+        path = path2DData +'/Noise {}'.format(scale)
+
+        if timestep == 0:
+            create_dir(path)
+
         noise = np.random.normal(0, scale, size=grid.x.size)
         grid['Height'] = z+noise
-        grid.save('data/mesh_i_{}_t_{}.vti'.format(i+1,timestep))
+        grid.save(path+'/mesh_t_{}.vti'.format(timestep))
+
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pyvista as pv
 from pyvista import examples
 from pyvista.trame.ui import plotter_ui
 from trame.app import get_server
-from trame.ui.vuetify import SinglePageWithDrawerLayout
+from trame.ui.vuetify import SinglePageWithDrawerLayout,VAppLayout
 from trame.widgets import vuetify
 import numpy as np
 
@@ -15,62 +15,66 @@ server = get_server()
 server.client_type='vue2'
 state, ctrl = server.state, server.controller
 
+
 server.hot_reload = True
 
 
-x = np.arange(-10, 10, 0.5)
-y = np.arange(-10, 10, 0.5)
-x, y = np.meshgrid(x, y)
-r = np.sqrt(x**2 + y**2)
-z = np.sin(r)
-
+def getFilePath(timestep,i):
+    return 'data/mesh_i_{}_t_{}.vti'.format(i,timestep)
 # Create and structured surface
-grid = pv.StructuredGrid(x, y, z)
-grid["Height"] = z.ravel()
+grid = pv.read(getFilePath(0,0))
+terrain=grid.warp_by_scalar()
 
-p = pv.Plotter()
+state['plotter']=pv.Plotter()
+p=state['plotter']
+print(p.shape)
+
 p.add_mesh(
-    grid,
+    terrain,
+    name='terrain',
     scalars="Height",
     lighting=False,
     show_edges=True,
-    clim=[-1, 1],
 )
 
 
-nframe = 15
-phases = np.linspace(0, 2 * np.pi, nframe + 1)[:nframe]
+timesteps = 15
+
+data_config={}
+data_config[0] = {
+    "title": "Reference",
+}
+data_config[1] = {
+    "title": "Noise .01",
+}
+data_config[2] = {
+    "title": "Noise .05",
+}
+data_config[3] = {
+    "title": "Noise .1",
+}
+
 
 @state.change("timestep")
 def slider_value_change(timestep, **kwargs):
-  phase=phases[timestep]
-  z = np.sin(r + phase)
-  grid.points[:, -1] = z.ravel()
-  grid["Height"] = z.ravel()
-  view.update()
-  print(phase,timestep)
+  grid = pv.read(getFilePath(timestep, 0))
+  terrain = grid.warp_by_scalar()
+  state['plotter'].add_mesh(
+      terrain,
+      name='terrain')
   print(f"Slider is changing slider_value to {timestep}")
+
+@state.change("show_second_graph")
+def toggle_show_second_graph(show_second_graph, **kwargs):
+  print("state change",show_second_graph)
+  state['disable_second_grid_radio_buttons']= not show_second_graph
+  if(show_second_graph):
+      return
 
 
 
 
 with SinglePageWithDrawerLayout(server) as layout:
-    with layout.toolbar:
-        vuetify.VSpacer()
-        vuetify.VCheckbox(
-            label="Log Scale",
-            v_model=("log_scale", False),
-            hide_details=True,
-            dense=True,
-            outlined=True,
-        )
-        vuetify.VSlider(
-            label="Timestep",
-            dense=True,
-            min=0,
-            max=nframe-1,
-            v_model=("timestep", 0),
-        )
     with layout.content:
         with vuetify.VContainer(
             fluid=True,
@@ -78,6 +82,32 @@ with SinglePageWithDrawerLayout(server) as layout:
         ):
             # Use PyVista UI template for Plotters
             view = plotter_ui(p)
+            print(view)
             ctrl.view_update = view.update
+    with layout.drawer:
+        with vuetify.VContainer(
+                fluid=True,
+                classes="d-flex flex-column p-2",
+        ):
+            with vuetify.VContainer(
+                    fluid=True,
+                    classes="d-flex flex-column p-2",
+            ):
+                vuetify.VSlider(
+                    label="Timestep",
+                    dense=True,
+                    min=0,
+                    max=timesteps - 1,
+                    v_model=("timestep", 0),
+                )
+                vuetify.VCheckbox(label='Show Second Graph',v_model=('show_second_graph',False))
+                with vuetify.VRadioGroup(
+                        v_model=("second_graph_grid", next(iter(data_config))),
+                        disabled=("disable_second_grid_radio_buttons",False)
+
+
+                ):
+                    for key, value in data_config.items():
+                        vuetify.VRadio(label=value["title"], value=key)
 
 server.start()
