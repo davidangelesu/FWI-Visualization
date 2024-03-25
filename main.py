784b0ceaf9013143bbeb85ce5f9d13340e479e85
David Angeles Ungson <davidangelesu@gmail.com>
2024-03-25 19:31:46 +0100

    migrated to vue3

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pyvista as pv
 from pyvista.trame.ui import plotter_ui
 from trame.app import get_server
-from trame.ui.vuetify import SinglePageWithDrawerLayout
+from trame.ui.vuetify3 import SinglePageWithDrawerLayout
 from trame.ui.router import RouterViewLayout
-from trame.widgets import vuetify,html,router
+from trame.widgets import vuetify3 as vuetify,html,router
 import os
 
 # ----find Parameters----
@@ -15,7 +15,7 @@ timesteps = len([filename for filename in os.listdir(path2DData + '/' + folders[
 pv.OFF_SCREEN = True
 
 server = get_server()
-server.client_type = 'vue2'
+server.client_type = 'vue3'
 state, ctrl = server.state, server.controller
 
 state['first_folder'] = folders[0]
@@ -117,8 +117,8 @@ def minus_timestep():
     state['timestep']=max(state['timestep']-1,0)
 
 
-def create_select(label:str,variable:str):
-    vuetify.VSelect(label=label,v_model=(variable,), items=("options",folders))
+def create_select(label:str,variable:str, **kwargs):
+    vuetify.VSelect(label=label,v_model=(variable,), items=("options",folders), **kwargs)
 
 
 
@@ -139,12 +139,16 @@ def create_side_by_side_layout():
                             classes="d-flex flex-column p-2",
                     ):
                         html.H3("Mode:")
-                        with vuetify.VBtnToggle(v_model=("plotMode", 0), mandatory=True,
+                        with vuetify.VBtnToggle(v_model=("plotMode", 0), mandatory=True,divided=True,  rounded="xs",
                                                 classes="d-flex w-100", ):
                             with vuetify.VBtn(value=0, size="small"):
                                 vuetify.VIcon("mdi-cube-send")
+                                vuetify.VTooltip("displacement field",activator="parent",  location="bottom")
+
                             with vuetify.VBtn(value=1, size="small"):
                                 vuetify.VIcon("mdi-cube-outline")
+                                vuetify.VTooltip(    "material field",activator="parent",  location="bottom")
+
                         html.H3("Visualization Tools:")
                         vuetify.VCheckbox(
                             label="Show Contour Plots",
@@ -158,23 +162,24 @@ def create_side_by_side_layout():
                                     label="Timestep",
                                     min=0,
                                     max=timesteps - 1,
+                                    step=1,
                                     v_model=("timestep",),
                                     disabled=("plotMode==1",)
                                 )
                             with vuetify.VCol(cols="2", classes="fill-height"):
                                 with vuetify.VContainer():
                                     with vuetify.VRow():
-                                        vuetify.VBtn("+", x_small=True, tile=True, click=add_timestep)
-
+                                        vuetify.VBtn("+", size="x-small", tile=True, rounded="0",click=add_timestep,elevation="0")
                                     with vuetify.VRow():
-                                        vuetify.VBtn("-", x_small=True, tile=True, click=minus_timestep)
+                                        vuetify.VBtn("-", size="x-small", tile=True,rounded="0", click=minus_timestep,elevation="0")
                         with vuetify.VContainer(
                                 fluid=True,
                                 classes="d-flex flex-row p-2",
                         ):
-                            create_select("Side A", 'first_folder')
-                            vuetify.VDivider()
-                            create_select("Side B", 'second_folder')
+
+                            create_select("Side A", 'first_folder', )
+
+                            create_select("Side B", 'second_folder',)
 
         with vuetify.VCol(cols="9", classes="fill-height"):
             with vuetify.VContainer(fluid=True, classes="fill-height"):
@@ -192,23 +197,23 @@ def create_about_layout():
     print("About goes here")
 
 
-with RouterViewLayout(server, "/"):
+with RouterViewLayout(server, "/",name="content"):
     create_side_by_side_layout()
 
 
 
-with RouterViewLayout(server,"/plot"):
+with RouterViewLayout(server,"/plot",name="content"):
     create_plot_layout()
 
 
-with RouterViewLayout(server,"/about"):
+with RouterViewLayout(server,"/about",name="content"):
     create_about_layout()
 
 
 with SinglePageWithDrawerLayout(server) as layout:
     layout.title.set_text("Full Waveform Inversion Results Visualizer")
     with layout.content:
-        router.RouterView(classes="w-100 fill-height")
+        router.RouterView(name="content",classes="w-100 h-100")
     with layout.drawer:
         with vuetify.VContainer(
                 fluid=True,
@@ -219,5 +224,4 @@ with SinglePageWithDrawerLayout(server) as layout:
             vuetify.VBtn("About",text=True,to="/about")
 
 
-
-server.start(port=8081)
+server.start()
